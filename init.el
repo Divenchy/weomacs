476eac96ddafff45ecc5f3a3e36d0a0155e9e9db
Divenchy <lfexp35@gmail.com>
2026-09-14 20:47:17 -0500

    removing add-to-list

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (load-file "~/.emacs.d/basic_settings.el")
 
 ;; Treesit-sexp by Alexis Purslane
-(add-to-list load-file "~/.emacs.d/treesit-sexp")
+(load-file "~/.emacs.d/treesit-sexp")
 
 ;; Emacs remaps
 (load-file "~/.emacs.d/remaps.el")
