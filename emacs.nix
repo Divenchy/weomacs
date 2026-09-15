91dd8e0bcec7c99faf32e6717d0883dc01c5d295
Divenchy <lfexp35@gmail.com>
2026-09-14 20:58:01 -0500

    removing treesit-sexp from loading, not working for some reason

--- emacs.nix
+++ emacs.nix
@@ -163,8 +163,6 @@ in {
     ".emacs.d/remaps.el".source = ./remaps.el;
     ".emacs.d/themes.el".source = ./themes.el;
     ".emacs.d/org.el".source = ./org.el;
-    ".emacs.d/treesit-sexp/treesit-sexp.el".source = ./treesit-sexp/treesit-sexp.el;
-    ".emacs.d/treesit-sexp/structural-editing.el".source = ./treesit-sexp/structural-editing.el;
     ".emacs.d/lsp/lsp.el".source = ./lsp/lsp.el;
     ".emacs.d/lsp/debugging.el".source = ./lsp/debugging.el;
     ".emacs.d/lsp/langs/zig.el".source = ./lsp/langs/zig.el;
